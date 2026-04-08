50a2f1bb9a952f271638fc8761bb539023c06ed9
Gabriel Dornas <gabrielbdornas@gmail.com>
2026-04-08 15:01:42 -0300

    docs(code): add namedTuple with types

---
+++ main_type.py
@@ -0,0 +1,13 @@
+from typing import NamedTuple, Dict
+
+
+class Pessoa(NamedTuple):
+    nome: str
+    sobrenome: str
+    telefone: Dict[str, str]
+    ddd: int
+
+
+eduardo_1 = Pessoa('Eduardo', 8, {'residencial': '1111-111', 'móvel': '999-999-999'}, 19)
+eduardo_2 = Pessoa('Eduardo', 8, {'residencial': '1111-111', 'móvel': '999-999-999'}, 19)
+print(type(eduardo_1.telefone))
